49bb60bda71d714fa81b873935ddebb1fee56af2
xqxyxchy <xqxyxchy@126.com>
2025-07-09 14:52:07 +0800

    【优化】新增wandb参数

--- trainer/train_pretrain_vlm.py
+++ trainer/train_pretrain_vlm.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     torch.manual_seed(1337)
     device_type = "cuda" if "cuda" in args.device else "cpu"
 
-    ts=datetime.now(ZoneInfo("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S")
+    ts=datetime.now(ZoneInfo("Asia/Shanghai")).strftime("%Y-%m-%d_%H:%M:%S")
     args.wandb_run_name = f"{ts}-Epoch-{args.epochs}-BatchSize-{args.batch_size}-LearningRate-{args.learning_rate}"
 
     ctx = nullcontext() if device_type == "cpu" else torch.cuda.amp.autocast()
